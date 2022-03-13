adf25a1acf4db017e753c7c1a0d71b9830551dbb
charon <cariasjosue@gmail.com>
2022-03-13 18:11:57 -0400

    path updates

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { json } = require('express/lib/response');
 const app = express();
+const noteInput = require("./db/db.json");
 const PORT = process.env.PORT || 3000;
 const fs = require("fs");
 
