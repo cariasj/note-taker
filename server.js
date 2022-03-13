509d617ce04689b284c2ed916cc8bf086f26263a
charon <cariasjosue@gmail.com>
2022-03-13 17:15:16 -0400

    api get

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require('express');
+const { json } = require('express/lib/response');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const fs = require("fs");
+
+app.use(express.static("./develop/public"));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
-
-
-
-
-
-
-app.listen(PORT, function() {
-    console.log('Active on port ' + PORT);
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"))
 });
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
