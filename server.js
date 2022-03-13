9e3d3e1a3b1785182c4182ceeb14f099d7641277
charon <cariasjosue@gmail.com>
2022-03-13 18:52:02 -0400

    update src

--- server.js
+++ server.js
@@ -6,33 +6,33 @@ const path = require("path");
 const PORT = process.env.PORT || 3000;
 const fs = require("fs");
 
-app.use(express.static("./public"));
+app.use(express.static("/public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+
 
 app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./db/db.json"))
+    res.sendFile(path.join(__dirname, "/db/db.json"))
+
 });
 
 
 app.post("/api/notes", (req, res) => {
-    const noteInput = JSON.parse(fs.readFileSync("./db/db/json"));
+    const noteInput = JSON.parse(fs.readFileSync("/db/db/json"));
     const note = req.body;
     noteInput.push(note);
-    fs.writeFileSync("./db/db.json", JSON.stringify(noteInput))
+    fs.writeFileSync("/db/db.json", JSON.stringify(noteInput))
     res.json(noteInput);
 });
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
-
-
 
 
 
