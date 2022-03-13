7773c7264211c16ff41a01a3619fefb474156fd7
charon <cariasjosue@gmail.com>
2022-03-13 17:41:59 -0400

    routes

--- server.js
+++ server.js
@@ -15,6 +15,20 @@ app.get("/api/notes", (req, res) => {
 });
 
 
+app.post("/api/notes", (req, res) => {
+    const noteInput = JSON.parse(fs.readFileSync("./db/db/json"));
+    const note = req.body;
+    noteInput.push(note);
+    fs.writeFileSync("./db/db.json", JSON.stringify(noteInput))
+    res.json(noteInput);
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
 
 
 
