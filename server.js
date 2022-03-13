e9c1a8d232796b3995e249bfcae7cdba2beb3ecd
charon <cariasjosue@gmail.com>
2022-03-13 18:01:39 -0400

    html gets

--- server.js
+++ server.js
@@ -24,10 +24,10 @@ app.post("/api/notes", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 
